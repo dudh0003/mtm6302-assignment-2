6c1e1efd349eccba30823cb1e97db986986bdbe9
Keval Dudhat <dudh0003@algonquinlive.com>
2021-03-21 14:12:25 -0400

    finish assignment

---
+++ script.js
@@ -0,0 +1,47 @@
+let up = document.getElementById('GFG_UP');  
+        up.innerHTML = "Click on the button to show dynamic image element";  
+        let down = document.getElementById('GFG_DOWN');  
+          
+        function fun() { 
+            let img = document.createElement('img'); 
+		    let img1 = document.createElement('img'); 
+			let img2 = document.createElement('img'); 
+			let img3 = document.createElement('img'); 
+			let img4 = document.createElement('img'); 
+			let img5 = document.createElement('img'); 
+			let img6 = document.createElement('img'); 
+			let img7 = document.createElement('img'); 
+			let img8 = document.createElement('img'); 
+			let img9 = document.createElement('img'); 
+			let img10 = document.createElement('img'); 
+			let img11 = document.createElement('img'); 
+			let img12 = document.createElement('img'); 
+            img.src =  
+'https://static.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg',
+document.getElementById('body').appendChild(img); 
+img1.src= 'https://static.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg';  
+            document.getElementById('body').appendChild(img1); 
+			img2.src= 'https://images.pexels.com/photos/2553358/pexels-photo-2553358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';  
+            document.getElementById('body').appendChild(img2); 
+			img3.src= 'https://images.pexels.com/photos/4666751/pexels-photo-4666751.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';  
+            document.getElementById('body').appendChild(img3); 
+			img4.src= 'https://images.pexels.com/photos/45911/peacock-plumage-bird-peafowl-45911.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';  
+            document.getElementById('body').appendChild(img4); 
+			img5.src= 'https://images.pexels.com/photos/2317904/pexels-photo-2317904.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';  
+            document.getElementById('body').appendChild(img5); 
+			img6.src= 'https://static.pexels.com/photos/158471/ibis-bird-red-animals-158471.jpeg';  
+            document.getElementById('body').appendChild(img6); 
+			img7.src= 'https://static.pexels.com/photos/133459/pexels-photo-133459.jpeg';  
+            document.getElementById('body').appendChild(img7); 
+			img8.src= 'https://static.pexels.com/photos/50988/ape-berber-monkeys-mammal-affchen-50988.jpeg';  
+            document.getElementById('body').appendChild(img8); 
+			img9.src= 'https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';  
+            document.getElementById('body').appendChild(img9); 
+			img10.src= 'https://images.pexels.com/photos/2322425/pexels-photo-2322425.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';  
+            document.getElementById('body').appendChild(img10); 
+			img11.src= 'https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';  
+            document.getElementById('body').appendChild(img11); 
+			 
+			
+              
+        }  
